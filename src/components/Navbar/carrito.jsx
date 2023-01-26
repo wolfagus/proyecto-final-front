27fc8c34c69bea96e3a0f88827a718061100e42d
BeeSluka <valeriasluka@gmail.com>
2023-01-26 01:56:38 -0300

    Conexion DB

---
+++ src/components/Navbar/carrito.jsx
@@ -0,0 +1,10 @@
+import { FaShoppingBasket } from "react-icons/fa";
+
+const Carrito = () => {
+  return (
+    <div>
+      <FaShoppingBasket color="white" />
+    </div>
+  );
+};
+export default Carrito;
