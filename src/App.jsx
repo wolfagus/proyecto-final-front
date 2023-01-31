dae3f97effa6d12a046604e9459aca8894c6a061
agustin <agustinmercado123@gmail.com>
2023-01-31 13:02:57 -0300

    se soluciona el problema de que en el contexState no se modificaban los valores

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { ActionTypes, useContextState } from './context/contextState';
 import PublicRoutes from './routes/PublicRoutes';
+import { getLocalStorage } from './utils/localStorageHelper';
+import { useEffect } from 'react';
 
 
 function App() {
+  const { setContextState } = useContextState();
+
+  useEffect(() => {
+    const haveUser = getLocalStorage('token');
+    if (haveUser) {
+      setContextState({ type: ActionTypes.SET_USER_LOGIN, value: true });
+    }
+    
+  }, []);
   return (
     <BrowserRouter>
       <PublicRoutes/>
